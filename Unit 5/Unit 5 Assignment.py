9270477300dc3538c9c8902a45825ed8082fd6be
Alec Akin <alec@Alecs-MacBook-Pro.local>
2022-03-10 23:38:28 -0700

    Finished assignment (finally....)
    Finished assignment, renamed, and added all required features.

--- Unit 5/Unit 5 Assignment.py
+++ Unit 5/Unit 5 Assignment.py
@@ -7,38 +7,49 @@ https://www.github.com/rainmana/PUG-IT244
 '''
 
 
-#Import extra libraries to achieve bonus functionality/features that go beyond Assignment Requirements
+#Import libraries
+from ast import Index
 import sys
-import csv
 
-#Define variables to be used later
-recordCount = 0
-recordCount = int(recordCount)
-recordsList = []
 
-#Instead of using a static filename in inline code, make the progam able to take in any text file from the command line as arg
+
+#Initialize variable for use later
+recordCounter = 0
+recordCounter = int(recordCounter)
+recordsList = list
+
+#Instead of reading a static file name/location, read in file as command line argument
 with open(sys.argv[1], 'r') as file:
-    #Use for loop to add records from text file line by line to recordsList
-    for line in file:
-        recordsList = recordsList.append(line.split())
-        #No close statement necessary since it's automatically taken care of. With statement makes sure this is done automatically.
+   contents = file.readlines()
+   fileName = sys.argv[1]
 
-print(recordsList)
-sys.exit()
-#Appned a new record to the list as a string of data
-recordsList.append("5,Brady,Bobby,422 Clinton Way,Los Angeles,CA")
+#Add the contents of the file to the recordsList list and strip lines with for loop
+recordsList = [line.strip() for line in contents]
+
+#Add a fifth record to recordsList
+recordsList.append("5,Brady,Bobby,4222 Clinton Way,Los Angeles,CA")
+
+#Count number of records in recordsList and assign it to recordCount and recordCounter for the next Loop
+recordCounter = len(recordsList)
+recordCount = len(recordsList)
+
+#Write out the now updated recordsList list to a new file with a counter that increases and compares to a static record value to close the while loop
+outputFile = open("IT244_U5_PromoCredit.csv", "w")
+outputFile.write("Customer ID, Last Name, First Name, Address, City, State, Promo Credit\n")
+while recordCounter <= recordCount:
+        for index in recordsList:
+            outputFile.write(index)
+            outputFile.write(",$500\n")
+            recordCounter = recordCounter + 1
+
+#Out put data to stdout that includes the recordCount, the input filename, and records processed
+print('There were',recordCount,'files written to the prmo credits csv file.\n')
+print('\nINPUT: ')
+print(fileName,'\n')
+
+for index in recordsList:
+    print(index)
 
 
-
-
-#Write data to a CSV file in the directory this script was run in
-with open('Unit5_Assignment_Output.csv', 'w', newline='') as csvFile:
-    writer = csv.writer(csvFile, delimiter=',')
-    writer.writerow(['Customer ID','Last Name','First Name','Address','City','State','Promo Credit','test'])
-    for index,record in enumerate(recordsList):
-        writer.writerow(recordsList)
-        recordCount = recordCount + len(recordsList)
-        print('The record count now is: ', recordCount)
-print(recordsList)
 
 
